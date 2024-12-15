8ca23b33700329a10903b8c4fec9760d0a41ead5
Tom Okafor <tmeka008@gmail.com>
2024-12-15 19:39:35 +0100

    Update family tracker project.

--- 8.5 Family Travel Tracker/index.js
+++ 8.5 Family Travel Tracker/index.js
@@ -9,7 +9,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
-  password: "123456",
+  password: "51017991",
   port: 5432,
 });
 db.connect();
@@ -18,14 +18,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let currentUserId = 1;
-
-let users = [
-  { id: 1, name: "Angela", color: "teal" },
-  { id: 2, name: "Jack", color: "powderblue" },
+const COLORS = [
+  "#db2828",
+  "#f2711c",
+  "#b5cc18",
+  "#21ba45",
+  "#6435c9",
+  "#e03997",
 ];
 
+/*let users = [
+  { id: 1, name: "Angela", color: "teal" },
+  { id: 2, name: "Jack", color: "powderblue" },
+];*/
+async function getUsers() {
+  let users = await db.query("SELECT * FROM users");
+  users = users.rows;
+  return users;
+}
+console.log(await getUsers());
+
 async function checkVisisted() {
-  const result = await db.query("SELECT country_code FROM visited_countries");
+  const result = await db.query(
+    "SELECT country_code FROM visited_countries WHERE user_id = $1",
+    [currentUserId]
+  );
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
@@ -34,43 +51,89 @@ async function checkVisisted() {
 }
 app.get("/", async (req, res) => {
   const countries = await checkVisisted();
+  const USERS = await getUsers();
+  const INDEX = USERS.findIndex((user) => user.id === currentUserId);
   res.render("index.ejs", {
     countries: countries,
     total: countries.length,
-    users: users,
-    color: "teal",
+    users: USERS,
+    color: USERS[INDEX].color,
+    INDEX,
   });
 });
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
-
-  try {
-    const result = await db.query(
-      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
-      [input.toLowerCase()]
-    );
-
-    const data = result.rows[0];
-    const countryCode = data.country_code;
+  if (input === "") {
+    sendErrorMessage(req, res, "Please, enter a country.");
+  } else {
     try {
-      await db.query(
-        "INSERT INTO visited_countries (country_code) VALUES ($1)",
-        [countryCode]
+      const result = await db.query(
+        "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE  $1 || '%';",
+        [input.toLowerCase()]
       );
-      res.redirect("/");
+      const data = result.rows[0];
+      const countryCode = data.country_code;
+      try {
+        await db.query(
+          "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
+          [countryCode, currentUserId]
+        );
+        res.redirect("/");
+      } catch (err) {
+        await sendErrorMessage(
+          req,
+          res,
+          "You have already entered this country."
+        );
+        console.log(err);
+      }
     } catch (err) {
+      await sendErrorMessage(
+        req,
+        res,
+        "The country you entered does not exist."
+      );
       console.log(err);
     }
-  } catch (err) {
-    console.log(err);
   }
 });
-app.post("/user", async (req, res) => {});
+app.post("/user", async (req, res) => {
+  if (req.body.user) {
+    currentUserId = +req.body.user;
+    res.redirect("/");
+  } else {
+    res.render("new.ejs");
+  }
+});
 
 app.post("/new", async (req, res) => {
   //Hint: The RETURNING keyword can return the data that was inserted.
   //https://www.postgresql.org/docs/current/dml-returning.html
+  const NAME = req.body.name;
+  let color = req.body.color;
+  if (!color) {
+    color = COLORS[Math.floor(Math.random() * COLORS.length)];
+  }
+  await db.query("INSERT INTO users (name, color) VALUES ($1, $2)", [
+    NAME,
+    color,
+  ]);
+  res.redirect("/");
 });
+
+async function sendErrorMessage(req, res, errorMessage) {
+  const countries = await checkVisisted();
+  const USERS = await getUsers();
+  const INDEX = USERS.findIndex((user) => user.id === currentUserId);
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    users: USERS,
+    color: USERS[INDEX].color,
+    INDEX,
+    error: errorMessage,
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
