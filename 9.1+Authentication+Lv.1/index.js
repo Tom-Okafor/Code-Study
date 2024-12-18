d77faa98810f703ba3e2c6dadb6561c5b1eb0a85
Tom Okafor <tmeka008@gmail.com>
2024-12-18 12:14:28 +0100

    Connect to database and initiate password check.

--- 9.1+Authentication+Lv.1/index.js
+++ 9.1+Authentication+Lv.1/index.js
@@ -1,12 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
+const { Client } = pg;
 const app = express();
 const port = 3000;
+const DB_PASSWORD = process.env.password;
+
+const DATABASE = new Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: DB_PASSWORD,
+  port: 5432,
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
+await DATABASE.connect();
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
@@ -19,10 +32,39 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  const EMAIL = req.body.username;
+  const PASSWORD = req.body.password;
+  let newUser = await DATABASE.query(
+    "INSERT INTO users (email, password) VALUES($1, $2) RETURNING *",
+    [EMAIL, PASSWORD]
+  );
+  newUser = newUser.rows;
+  console.log(newUser);
+});
 
-app.post("/login", async (req, res) => {});
+app.post("/login", async (req, res) => {
+  const EMAIL = req.body.username;
+  const PASSWORD = req.body.password;
+  try {
+    let loginPassword = await DATABASE.query(
+      "SELECT password FROM users WHERE email = $1",
+      [EMAIL]
+    );
+    loginPassword = loginPassword.rows;
+    console.log(loginPassword);
+    console.log(PASSWORD);
+
+    if (loginPassword[0].password === PASSWORD) {
+      res.send("CORRECT!");
+    } else {
+      res.send("OOPS! TRY AGAIN");
+    }
+  } catch (error) {
+    res.send("Email not found");
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port http://localhost:${port}`);
 });
